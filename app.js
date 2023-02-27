cd7510cf876f60741dcdf919e9c0aaa79c15831c
NivasReddy2400 <nivasreddy2401@gmail.com>
2023-02-28 00:51:28 +0530

    2nd

---
+++ app.js
@@ -0,0 +1,98 @@
+const quizContainer = document.querySelector('.container');
+const question = document.querySelector('.question');
+const next = document.querySelector('#next');
+const previous = document.querySelector('#previous');
+const ans1 = document.querySelector('#ans1');
+const ans2 = document.querySelector('#ans2');
+const ans3 = document.querySelector('#ans3');
+const ans4 = document.querySelector('#ans4');
+const submitBtn = document.querySelector('#submit');
+
+let quiz = [];
+let currentQuestionIndex = 0;
+let score = 0;
+
+fetch('https://opentdb.com/api.php?amount=20&category=18&difficulty=hard&type=multiple')
+  .then(response => response.json())
+  .then(data => {
+    quiz = data.results;
+    displayCurrentQuestion();
+  })
+  .catch(error => console.error(error));
+
+function displayCurrentQuestion() {
+  const currentQuestion = quiz[currentQuestionIndex];
+  question.textContent = currentQuestion.question;
+  let crctans = currentQuestion.correct_answer;
+  let incrctans = currentQuestion.incorrect_answers;
+  let optionList = incrctans.slice();
+  optionList.splice(Math.floor(Math.random() * (incrctans.length + 1)), 0, crctans);
+  ans1.textContent = optionList[0];
+  ans2.textContent = optionList[1];
+  ans3.textContent = optionList[2];
+  ans4.textContent = optionList[3];
+}
+
+function checkAnswer(selectedAnswer) {
+  const currentQuestion = quiz[currentQuestionIndex];
+  const correctAnswer = currentQuestion.correct_answer;
+  if (selectedAnswer === correctAnswer) {
+    score++;
+  }
+}
+
+function handleNextClick() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex >= quiz.length) {
+      currentQuestionIndex = 0;
+    }
+    displayCurrentQuestion();
+  }
+  
+  function handlePreviousClick() {
+    currentQuestionIndex--;
+    if (currentQuestionIndex < 0) {
+      currentQuestionIndex = quiz.length - 1;
+    }
+    displayCurrentQuestion();
+  }
+
+ans1.addEventListener('click', function() {
+  checkAnswer(ans1.textContent);
+  currentQuestionIndex++;
+  displayCurrentQuestion();
+});
+
+ans2.addEventListener('click', function() {
+  checkAnswer(ans2.textContent);
+  currentQuestionIndex++;
+  displayCurrentQuestion();
+});
+
+ans3.addEventListener('click', function() {
+  checkAnswer(ans3.textContent);
+  currentQuestionIndex++;
+  displayCurrentQuestion();
+});
+
+ans4.addEventListener('click', function() {
+  checkAnswer(ans4.textContent);
+  currentQuestionIndex++;
+  displayCurrentQuestion();
+});
+
+
+
+next.addEventListener('click', handleNextClick);
+previous.addEventListener('click', handlePreviousClick);
+
+submitBtn.addEventListener('click', function() {
+  const result = document.createElement('p');
+  result.textContent = `You scored ${score} out of ${quiz.length}`;
+  quizContainer.appendChild(result);
+  setTimeout(()=>{
+    result.textContent = " "
+  },3000)
+});
+
+
--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-const qapi = fetch('https://opentdb.com/api.php?amount=4&category=18&difficulty=hard&type=multiple')
-.then(res => res.json())
-.then(data => {
-    displayQuestion(data.results[0]);
+const quizContainer = document.querySelector('.container');
+const question = document.querySelector('.question');
+const ans1 = document.querySelector('#ans1');
+const ans2 = document.querySelector('#ans2');
+const ans3 = document.querySelector('#ans3');
+const ans4 = document.querySelector('#ans4');
+const submitBtn = document.querySelector('#submit');
+
+let quiz = [];
+let currentQuestionIndex = 0;
+let score = 0;
+
+fetch('https://opentdb.com/api.php?amount=20&category=18&difficulty=hard&type=multiple')
+.then(res=>{
+    return res.json();
+}).then((data) => {
+    quiz = data.results;
+    displayCurrentQuestion();
+    // console.log(quiz)
+}).catch((err) => {
+    console.log(err);
 })
 
-function displayQuestion(data){
-    let crctans = data.correct_answer;
-    let incrctans = data.incorrect_answers;
-    let optionList = incrctans;
-    optionList.splice(Math.floor(Math.random()*(incrctans.len+1)),0,crctans);
-    console.log(optionList)
+function displayCurrentQuestion(){
+    let currentQuestion = quiz[currentQuestionIndex];
+    question.innerHTML = currentQuestion.question;
+    let correctAnswer = currentQuestion.correct_answer;
+    let incorrectAnswers = currentQuestion.incorrect_answers;
+    let optionList = incorrectAnswers.slice(Math.floor(Math.random()*incorrectAnswers.length+1),0,correctAnswer)
+    console.log(optionList);
+
 }
-
-function checkans(){
-    
-}
- 
-
-
-
-// const next = document.querySelector('.next');
-// const previous = document.querySelector('.previous');
-// const question = document.querySelector('.question');
-// const ans1 = document.querySelector('#ans1');
-// const ans2 = document.querySelector('#ans2');
-// const ans3 = document.querySelector('#ans3');
-// const ans4 = document.querySelector('#ans4');
